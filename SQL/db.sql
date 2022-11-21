42a61f934743414ff323e4c0c6738310e617c344
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2022-11-21 12:04:03 +0100

    little modfi

--- SQL/db.sql
+++ SQL/db.sql
@@ -24,3 +24,22 @@ CREATE TABLE expenses(
    FOREIGN KEY(id_category) REFERENCES categories(id_category),
    FOREIGN KEY(id_user) REFERENCES users(id_user)
 );
+
+
+CREATE TABLE incomes(
+   id_income INT NOT NULL AUTO_INCREMENT,
+   name VARCHAR(100),
+   amount DECIMAL(15,2),
+   created_at DATE,
+   id_user INT NOT NULL,
+   id_recurence INT,
+   PRIMARY KEY(id_income),
+   FOREIGN KEY(id_user) REFERENCES users(id_user),
+   FOREIGN KEY(id_recurence) REFERENCES recurences(id_recurence)
+);
+
+CREATE TABLE recurences(
+   id_recurence COUNTER,
+  	period ENUM('week','month') NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
+   PRIMARY KEY(id_recurence)
+);
