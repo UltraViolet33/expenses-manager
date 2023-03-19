b53b23303a03a108cb25e2f816fce7029cc28457
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-03-19 14:26:00 +0100

    add selectbyid and update methodsé

--- website/core/models/Category.php
+++ website/core/models/Category.php
@@ -16,10 +16,24 @@ class Category
         return $this->db->read($sql);
     }
 
-    
+
+    public function selectById(int $id): object
+    {
+        $sql = "SELECT * FROM categories WHERE id_category = :id";
+        return $this->db->readOneRow($sql, ["id" => $id]);
+    }
+
+
     public function create(string $name): bool
     {
         $sql = "INSERT INTO categories(name) VALUES(:name)";
         return $this->db->write($sql, ["name" => $name]);
+    }
+
+
+    public function update(array $data): bool
+    {
+        $sql = "UPDATE categories SET name = :name WHERE id_category = :id";
+        return $this->db->write($sql, $data);
     }
 }
